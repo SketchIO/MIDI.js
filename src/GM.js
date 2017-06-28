677137812cb009e492fe9873f5cc850129bcc2c8
Kenneth Pullen <ken@mash.is>
2017-06-28 13:46:29 -0700

    A new scheme for accessing GM note and program data
    `GM.note[IDENTIFIER]` will return a bundle of information about the
    given note. The IDENTIFIER can be a note name ("AO") or note number
    (0-127). This makes converting anything to a note number pretty uniform:
    
        GM.note[IDENTIFIER].noteID
    
    The note bundle also has an array of key names associated with that note
    number ("C#", "Db"). The keys are always an array, even if there is only
    one key associated with a note.
    
    `GM.program[IDENTIFIER]` will return a bundle of information about a
    given program. Programs can only be searched by their name or programID
    (0-127). The program bundle does contain the program's family name,
    however.

--- src/GM.js
+++ src/GM.js
@@ -1,86 +1,51 @@
-import Debug from 'debug'
-const debug = Debug('MIDI.js:GeneralMIDI')
+/**
+ * @file General MIDI note and program mappings
+ * [1]:
+ *   http://www.electronics.dit.ie/staff/tscarff/Music_technology/midi/midi_note_numbers_for_octaves.htm
+ * [2]: https://www.midi.org/specifications/item/gm-level-1-sound-set
+ */
 
-const programDB = {
-	byID: {},
-	byName: {}
+import {ProgramInfo} from "./ProgramInfo"
+
+export const GM = {
+	note: {},
+	program: {}
 }
 
-function toName(name) {
-	return name.replace(/[^a-z0-9_ ]/gi, '').replace(/[ ]/g, '_').toLowerCase()
+const keymap = [["C"], ["C#", "Db"], ["D"], ["D#", "Eb"], ["E"], ["F"], ["F#", "Gb"], ["G"], ["G#", "Ab"], ["A"], ["A#", "Bb"], ["B"]]
+for (let noteID = 0; noteID < 128; noteID++) {
+	const octave = (noteID - 12) / 12 + 1 >> 0
+	const keys = keymap[noteID % 12]
+
+	const bundle = {
+		noteID,
+		octave,
+		keys
+	}
+
+	GM.note[noteID] = bundle
+	keys.forEach(function (key) {
+		const name = "" + key + octave
+		GM.note[name] = bundle
+	})
 }
 
-//(function buildProgramDatabase(categories) {
-//	for (var category in categories) {
-//		var programs = categories[category];
-//		for (var i = 0, length = programs.length; i < length; i++) {
-//			var program = programs[i];
-//			if (program) {
-//				const [idString, ...labelParts] = program.split(' ')
-//				const id = parseInt(idString, 10)
-//				const label = labelParts.join(' ')
-//
-//				var name = toName(label);
-//				var categoryId = toName(category);
-//
-//				var res = {
-//					id: --id,
-//					name: label,
-//					nameId: name,
-//					category: category
-//				};
-//
-//				byId[id] = res;
-//				byName[name] = res;
-//
-//				GM[categoryId] = GM[categoryId] || [];
-//				GM[categoryId].push(res);
-//			}
-//		}
-//	}
-//})(require('./GeneralMIDI.programInfo.json'))
+for (let family in ProgramInfo) {
+	const programs = ProgramInfo[family]
+	for (let i = 0; i < programs.length; i++) {
+		const program = programs[i]
+		const [idString, ...nameParts] = program.split(" ")
 
-const A0 = 0x15 // first note
-const C8 = 0x6C // last note
-const number2key = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
-const noteDB = {
-	toNote: {},
-	toName: {}
-}
-// TODO serious issues here.
-for (var n = A0; n <= C8; n++) {
-	let octave = (n - 12) / 12 >> 0;
-	let name = number2key[n % 12] + octave;
-	noteDB.toNote[name] = n;
-	noteDB.toName[n] = name;
-}
+		const programID = parseInt(idString, 10) - 1
+		const name = nameParts.join(" ")
 
-// console.log(noteDB)
-
-
-export default {
-	getNoteName(value) {
-		if (value in noteDB.toNote) {
-			return value
-		} else if (value in noteDB.toName) {
-			return noteDB.toName[value];
+		const bundle = {
+			programID,
+			family,
+			name,
 		}
-	},
 
-	getNoteNumber(value) {
-		if (value in noteDB.toName) {
-			return value
-		} else if (value in noteDB.toNote) {
-			return noteDB.toNote[value]
-		}
-		return value
-	},
-
-	getProgram(program) {
-		if (typeof program === 'string') {
-			return programDB.byName[toName(program)]
-		} else {
-			return programDB.byID[program]
-		}
+		GM.program[name] = bundle
+		GM.program[programID] = bundle
 	}
 }
