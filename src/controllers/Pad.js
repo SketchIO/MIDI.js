68e95b2739724a70c89b9e7de1c2792d74713d64
Kenneth Pullen <ken@mash.is>
2017-06-28 13:51:04 -0700

    Put controllers near each other

--- src/controllers/Pad.js
+++ src/controllers/Pad.js
@@ -1,17 +1,14 @@
-import filter from '../fn/filter'
-import MIDI from '../MIDI'
-import GM from '../GM'
+import {MIDI} from "../MIDI"
+import {filter} from "../fn"
+import {GM} from "../GM"
 
-import Debug from 'debug'
-const debug = Debug('MIDI.js:src/controllers/Pad.js')
-
-export default class Pad {
+export class Pad {
 	constructor(button2note) {
 		this.button2note = button2note
 	}
 
 	press(button) {
-		if(!MIDI.soundModule) return
+		if (!MIDI.soundModule) return
 		if (button in this.button2note) {
 			const {note, channelID = 0, requiresInteraction = false, maxSimultaneous = 1} = this.button2note[button]
 			if (note) {
@@ -24,7 +21,7 @@ export default class Pad {
 					}
 
 					if (activeNotes.length > maxSimultaneous) {
-						debug('There are too many simulteneous sounds (%s); canceling the first sound.', activeNotes.length)
+						debug("There are too many simulteneous sounds (%s); canceling the first sound.", activeNotes.length)
 						activeNotes[0].cancelImmediately()
 					}
 				}
@@ -33,9 +30,9 @@ export default class Pad {
 	}
 
 	release(button) {
-		if(!MIDI.soundModule) return
+		if (!MIDI.soundModule) return
 		if (button in this.button2note) {
-			debug('Releasing button %s', button)
+			debug("Releasing button %s", button)
 			const {note, channelID = 0} = this.button2note[button]
 			const noteID = GM.getNoteNumber(note)
 			MIDI.noteOff(channelID, noteID)
@@ -43,18 +40,18 @@ export default class Pad {
 	}
 
 	startInteractingWith(button) {
-		if(!MIDI.soundModule) return
+		if (!MIDI.soundModule) return
 		if (button in this.button2note) {
-			debug('Starting interaction with button %s', button)
+			debug("Starting interaction with button %s", button)
 			const {channelID = 0} = this.button2note[button]
 			MIDI.channels[channelID].volume = 127
 		}
 	}
 
 	stopInteractingWith(button) {
-		if(!MIDI.soundModule) return
+		if (!MIDI.soundModule) return
 		if (button in this.button2note) {
-			debug('Ending interaction with button %s', button)
+			debug("Ending interaction with button %s", button)
 			const {channelID = 0} = this.button2note[button]
 			MIDI.channels[channelID].volume = 0
 		}
