98da9b0ccea5795a41e13c415e6a27c0e8e0233d
Kenneth Pullen <kenneth.pullen@gmail.com>
2020-10-30 12:00:07 -0700

    Fix basic example

--- examples/basic.js
+++ examples/basic.js
@@ -5,40 +5,46 @@ global.MIDI = MIDI
 async function startup() {
 	await MIDI.autoconnect()
 	MIDI.channels = 1
-}
 
+}
 async function main() {
 	const {default: program} = await import('./soundfont/acoustic_grand_piano-ogg.json')
-	await MIDI.programs.load({
+	MIDI.programs.load({
 		programID: 0,
 		program
 	})
 
-	let segmentLength = 2
-	const notes = ['A', 'C#', 'G', 'B', 'F#', 'D']
+	MIDI.jobs.wait().then(function () {
+		let segmentlength = 2
+		const notes = ['A', 'C#', 'G', 'B', 'F#', 'D']
 
-	const now = MIDI.currentTime
-	let i
-	const noteBase = notes[Math.round(Math.random() * (notes.length - 1))]
-	for (i = 0; i < 8; i += 1) {
-		const note = noteBase + i
-		const timeslice = (segmentLength / 4)
-		const timestamp = now + timeslice * i
-		console.log({note, timeslice, timestamp})
-		MIDI.noteOn(0, note, 127, timestamp)
-	}
-
-	if (segmentLength > 0) {
-		MIDI.after(now + segmentLength, play)
-		segmentLength -= 1
-	} else {
-		MIDI.after(now + segmentLength, function () {
-			for (let i = 0; i < 8; i += 1) {
-				const note = 'B' + i
-				MIDI.noteOff(0, note, MIDI.currentTime + 1)
+		function play() {
+			const now = MIDI.currentTime
+			let i
+			const notebase = notes[Math.round(Math.random() * (notes.length - 1))]
+			for (i = 0; i < 8; i += 1) {
+				const note = notebase + i
+				const timeslice = (segmentlength / 4)
+				const timestamp = now + timeslice * i
+				console.log({note, timeslice, timestamp})
+				MIDI.noteOn(0, note, 127, timestamp)
 			}
-		})
-	}
+
+			if (segmentlength > 0) {
+				MIDI.after(now + segmentlength, play)
+				segmentlength -= 1
+			} else {
+				MIDI.after(now + segmentlength, function () {
+					for (let i = 0; i < 8; i += 1) {
+						const note = 'B' + i
+						MIDI.noteOff(0, note, MIDI.currentTime + 1)
+					}
+				})
+			}
+		}
+
+		play()
+	})
 }
 
 startup()
